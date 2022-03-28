822238ed7c8bbbe7f54038508dc3b9da920ce80a
sarmar11 <sara.martinezginer@telefonica.com>
2022-03-28 15:20:20 +0200

    fix unit tests

--- table_test.go
+++ table_test.go
@@ -13,7 +13,13 @@ var (
 	table2x1 = [][]string{{"name"}, {"John"}}
 	table2x2 = [][]string{{"name", "height"}, {"John", "182"}}
 	table2x3 = [][]string{{"name", "height", "age"}, {"John", "182", "32"}}
+	table3x2 = [][]string{{"param", "value"}, {"Name", "182"}, {"Height", "162"}}
 )
+
+type Headers struct {
+	Name   string
+	Height string
+}
 
 func TestRemoveHeaders(t *testing.T) {
 	tcs := []struct {
@@ -177,6 +183,44 @@ func TestConvertTableToMultiMap(t *testing.T) {
 			ctx := context.Background()
 			// Call the tested function
 			_, err := ConvertTableToMultiMap(ctx, tc.table)
+
+			// Check expected behavior
+			if err != nil && err.Error() != tc.expectedErr.Error() {
+				t.Errorf("Expected error: %s, Error received: %s", tc.expectedErr, err)
+			}
+
+			if tc.expectedErr == nil && err != nil {
+				t.Errorf("Expected error: %s, Error received: %s", tc.expectedErr, err)
+			}
+		})
+	}
+}
+
+func TestConvertTableWithoutHeaderToStruct(t *testing.T) {
+	tcs := []struct {
+		name        string
+		table       *godog.Table
+		expectedErr error
+	}{
+		{
+			name:        "Convert table wo header to struct ok",
+			table:       NewTable(table3x2),
+			expectedErr: nil,
+		},
+		{
+			name:  "Convert table wo header to struct fail",
+			table: NewTable(table2x2),
+			expectedErr: errors.New("failed setting element 'John' in struct of type " +
+				"'golium.Headers': field 'John' is not valid"),
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.Background()
+			var props Headers
+			// Call the tested function
+			err := ConvertTableWithoutHeaderToStruct(ctx, tc.table, &props)
 
 			// Check expected behavior
 			if err != nil && err.Error() != tc.expectedErr.Error() {
