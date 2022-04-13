0464bb10e02d08fee7481939a0332ce95f7d4cb4
Jordi Puig Bou <jordi.puigbou@telefonica.com>
2022-04-13 15:34:03 +0200

    fix: value sonar issues (#95)

--- value_test.go
+++ value_test.go
@@ -2,9 +2,20 @@ package golium_test
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/TelefonicaTC2Tech/golium"
+)
+
+const (
+	environmentPath = "./environments"
+	localConfFile   = `
+minio: true
+minioEndpoint: http://miniomock:9000
+`
+	testSHA256 = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
+	testBASE64 = "dGVzdA=="
 )
 
 func TestStringTag(t *testing.T) {
@@ -32,18 +43,59 @@ func TestSimpleNamedTag(t *testing.T) {
 	}
 }
 
-func TestComposedTag(t *testing.T) {
-	tcs := map[string]string{
-		"This is a test: [TRUE]":         "This is a test: true",
-		"[TRUE]: This is a test":         "true: This is a test",
-		"[TRUE]: This is a test.[FALSE]": "true: This is a test.false",
-		"This [EMPTY]boolean is [TRUE].": "This boolean is true.",
+func TestValuedTag(t *testing.T) {
+	tcs := map[string]interface{}{
+		"[TRUE]":  true,
+		"[FALSE]": false,
+		"[NULL]":  nil,
+		"[EMPTY]": "",
 	}
 	ctx := context.Background()
 	for s, expectedValue := range tcs {
+		v := golium.NewNamedTag(s).Value(ctx)
+		if v != expectedValue {
+			t.Errorf("expected: %s, actual: %s", expectedValue, v)
+		}
+	}
+}
+
+func TestComposedTag(t *testing.T) {
+	os.MkdirAll(environmentPath, os.ModePerm)
+	defer os.RemoveAll(environmentPath)
+
+	os.WriteFile("./environments/local.yml", []byte(localConfFile), os.ModePerm)
+	tcs := map[string]interface{}{
+		"[CONF:minio]":  true,
+		"[NUMBER:1]":    float64(1),
+		"[SHA256:test]": testSHA256,
+		"[BASE64:test]": testBASE64,
+		"[CTXT:test]":   "contextTest",
+	}
+
+	for s, expectedValue := range tcs {
+		ctx := golium.InitializeContext(context.Background())
+		if s == "[CTXT:test]" {
+			golium.GetContext(ctx).Put(golium.ValueAsString(ctx, "test"), "contextTest")
+		}
 		v := golium.NewComposedTag(s).Value(ctx)
 		if v != expectedValue {
 			t.Errorf("expected: %s, actual: %s", expectedValue, v)
 		}
+	}
+}
+
+func TestTagWithoutChecks(t *testing.T) {
+	tcs := []string{
+		"[NOW]",
+		"[UUID]",
+		"[NOW:1s]",
+		"[NOW:-1:]",
+		"[NOW:300ms:]",
+		"[NOW::]",
+		"[NOW::unix]",
+	}
+	ctx := context.Background()
+	for _, s := range tcs {
+		_ = golium.NewComposedTag(s).Value(ctx)
 	}
 }
