fc104e220a46ba95a8ab95bd75455a5c25f39dd9
ruben-garciad <ruben.garciadiaz@telefonica.com>
2025-09-03 16:38:01 +0200

    avoid HTTP parameter pollution

--- steps/dns/session.go
+++ steps/dns/session.go
@@ -23,7 +23,6 @@ import (
 	"io"
 	"net/http"
 	"net/url"
-	"strings"
 	"time"
 
 	"github.com/AdguardTeam/dnsproxy/upstream"
@@ -31,11 +30,33 @@ import (
 	"github.com/miekg/dns"
 )
 
-// Neutralize HTTP parameter pollution. CWE:235
-func neutralize(p string) string {
-	p = strings.ReplaceAll(p, "\r", "")
-	p = strings.ReplaceAll(p, "\n", "")
-	return p
+// SanitizeURLParams receives a URL as input and returns a sanitized version of it,
+// where the parameter values have been hardcoded to prevent malicious injections.
+// Avoid HTTP parameter pollution [CWE:235]
+func SanitizeURLParams(inputURL string) (string, error) {
+	parsedURL, err := url.Parse(inputURL)
+	if err != nil {
+		return "", fmt.Errorf("error parsing URL to sanitize it: %w", err)
+	}
+
+	// Get URL query parameters
+	queryParams := parsedURL.Query()
+
+	// Encode parameter values
+	for param, values := range queryParams {
+		for _, value := range values {
+			// The parameter value is encoded here to ensure that special characters are handled securely.
+			// special characters are handled safely.
+			// `url.QueryEscape` encodes the value so that it is safe to use in a URL.
+			// Then, the original value is replaced with the encoded value using the `Set` method.
+			queryParams.Set(param, url.QueryEscape(value))
+		}
+	}
+
+	// Establecer la cadena de consulta codificada en la URL
+	parsedURL.RawQuery = queryParams.Encode()
+
+	return parsedURL.String(), nil
 }
 
 // Session contains the information related to a DNS query and response.
@@ -159,23 +180,20 @@ func (s *Session) SendDoHQuery(
 		if errParse != nil {
 			return err
 		}
-
-		params := url.Values{}
-		for key, values := range s.DoHQueryParams {
-			for _, value := range values {
-				if !params.Has(key) {
-					params.Add(key, value)
-				}
-			}
-		}
-		u.RawQuery = neutralize(params.Encode())
+		params := url.Values(s.DoHQueryParams)
+		u.RawQuery = params.Encode()
 		urlStr = u.String()
 		bodyRequest = bytes.NewReader(data)
 	default:
 		return fmt.Errorf("unsupported method. %s", method)
 	}
 
-	request, err = http.NewRequest(method, neutralize(urlStr), bodyRequest)
+	sanitizedURL, err := SanitizeURLParams(urlStr)
+	if err != nil {
+		return err
+	}
+
+	request, err = http.NewRequest(method, sanitizedURL, bodyRequest)
 	if err != nil {
 		return err
 	}
